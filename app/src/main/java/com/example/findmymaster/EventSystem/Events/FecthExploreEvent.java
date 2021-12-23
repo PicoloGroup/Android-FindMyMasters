dec2651b45bd10853f43fc41f94fce9ab1415276
arda okçuoğlu <canardaokcuoglu@hotmail.com>
2021-12-23 22:07:12 +0300

    Arda : Fetch Explore event

---
+++ app/src/main/java/com/example/findmymaster/EventSystem/Events/FecthExploreEvent.java
@@ -0,0 +1,15 @@
+package com.example.findmymaster.EventSystem.Events;
+
+import com.example.findmymaster.EventSystem.EventBase;
+
+public class FecthExploreEvent extends EventBase {
+
+    public FecthExploreEvent(EventType type) {
+        super(type);
+    }
+
+    @Override
+    public String toString() {
+        return null;
+    }
+}
